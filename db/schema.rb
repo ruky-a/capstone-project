cae631a438e69aeef328085b54cc4e24502dc3cc
Your NameRuky <rukevwe009@gmail.com>
2019-11-05 18:20:09 +0000

    Revert "Revert "Add-stripe""
    This reverts commit 1efcecdd1ed6ae6eeb7dd99d13291a849c02f914.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_01_153440) do
+ActiveRecord::Schema.define(version: 2019_11_05_001938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,8 +116,8 @@ ActiveRecord::Schema.define(version: 2019_11_01_153440) do
     t.string "image"
     t.string "phone_number"
     t.text "description"
+    t.string "stripe_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
